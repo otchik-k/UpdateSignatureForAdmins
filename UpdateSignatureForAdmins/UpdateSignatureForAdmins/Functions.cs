3b86bcb80f3aca762a8bc0eb1d15188b80efa5ac
KIRILL OTCHIK <67601079+otchik-k@users.noreply.github.com>
2026-01-22 16:51:31 +0300

    Готовый скрипт

---
+++ UpdateSignatureForAdmins/UpdateSignatureForAdmins/Functions.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices.ActiveDirectory;
+using System.Globalization;
+using DirectoryEntry = System.DirectoryServices.DirectoryEntry;
+
+
+public class Functions
+{
+    public static Dictionary<string, string> GetAdUserAtributs(string ldapPath, string login)
+    {
+        Dictionary<string, string> resultDict = new Dictionary<string, string>
+        {
+            {"cn", "" },
+            {"title", "" },
+            {"company", "" },
+            {"streetAddress", "" },
+            {"l", "" },
+            {"mail", "" },
+            {"telephoneNumber", "" },
+            {"sAMAccountName", "" },
+            {"mobile", "" },
+            {"pager", "" }
+        };
+
+        using (DirectorySearcher searcher = new DirectorySearcher())
+        {
+            searcher.SearchRoot = new DirectoryEntry("LDAP://" + ldapPath);
+            searcher.Filter = $"sAMAccountName={login}";
+
+            searcher.PropertiesToLoad.Add("cn");
+            searcher.PropertiesToLoad.Add("title");
+            searcher.PropertiesToLoad.Add("company");
+            searcher.PropertiesToLoad.Add("streetAddress");
+            searcher.PropertiesToLoad.Add("l");
+            searcher.PropertiesToLoad.Add("mail");
+            searcher.PropertiesToLoad.Add("telephoneNumber");
+            searcher.PropertiesToLoad.Add("mobile");
+            searcher.PropertiesToLoad.Add("pager");
+
+
+            SearchResult result = searcher.FindOne();
+
+            if (result != null)
+            {
+                foreach (var key in resultDict.Keys)
+                {
+                    if (result.Properties.Contains(key) && result.Properties[key].Count > 0)
+                    {
+                        resultDict[key] = result.Properties[key][0].ToString();
+                    }
+                    else
+                    {
+                        resultDict[key] = null;
+                    }
+                }
+            }
+            else
+            {
+                resultDict["sAMAccountName"] = "tyty";
+                UsingStreamWriter("Пользователь" + login + " не найден.");
+            }
+        }
+        return resultDict;
+    }
+
+
+    public static string logName = "log\\log - " + DateTime.Now.ToString("dd.MM.yyyy HH.mm.ss") + ".txt";
+    public static void UsingStreamWriter(string anyString)
+    {
+        using (StreamWriter logWriter = new StreamWriter(logName, true))
+        {
+            logWriter.WriteLine(anyString);
+        }
+    }
+
+
+    public static string[] GetParametrValue(string configFileLine, string separator)
+    {
+        return configFileLine.Split(separator);
+    }
+
+
+    public static string GetSqlIdentityID(string connectToSQL, string cn, string email)
+    {
+        string result = "";
+        using (SqlConnection connection = new SqlConnection(connectToSQL))
+        {
+            connection.Open();
+            string queryText = $"SELECT identity_id FROM identities WHERE name=\'{cn}\' AND email=\'{email}\'";
+            using (SqlCommand command = new SqlCommand(queryText, connection))
+            {
+                try
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            result = reader.GetInt32(0).ToString();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    UsingStreamWriter(ex.Message);
+                    result = null;
+                }
+
+            }
+        }
+        return result;
+    }
+
+
+    public static void UpdateSqLSignature(string connectToSQL, string signatureText, string identity_id)
+    {
+        using (SqlConnection con = new SqlConnection(connectToSQL))
+        {
+            con.Open();
+            string queryText = "UPDATE identities SET signature=@signature, html_signature=1  WHERE identity_id=@identity_id";
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand(queryText, con))
+                {
+                    cmd.Parameters.AddWithValue("@signature", signatureText);
+                    cmd.Parameters.AddWithValue("@identity_id", identity_id);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                }
+            }
+            catch (SqlException ex)
+            {
+                UsingStreamWriter(ex.Message);
+            }
+        }
+    }
+
+
+    public static string GetSqlSignature(string connectToSQL, string identity_id)
+    {
+        string result = "";
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(connectToSQL))
+            {
+                connection.Open();
+                string queryText = "SELECT signature FROM identities WHERE identity_id=@identity_id";
+                using (SqlCommand command = new SqlCommand(queryText, connection))
+                {
+                    command.Parameters.AddWithValue("@identity_id", identity_id.ToString());
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            result = reader.GetString(0);
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+        catch (Exception ex)
+        {
+            UsingStreamWriter(ex.Message);
+            return null;
+        }
+    }
+
+
+    public static List<string> GetAllLoginAd(string ldapPath)
+    {
+
+        using (DirectoryEntry entry = new DirectoryEntry("LDAP://" + ldapPath))
+        {
+            try
+            {
+                DirectorySearcher searcher = new DirectorySearcher(entry);
+                searcher.Filter = "(objectClass=user)";
+                searcher.PropertiesToLoad.Add("sAMAccountName");
+                SearchResultCollection results = searcher.FindAll();
+
+                List<string> samAccountNames = new List<string>();
+                foreach (SearchResult result in results)
+                {
+                    if (result.Properties.Contains("sAMAccountName"))
+                    {
+                        string samAccountName = (string)result.Properties["sAMAccountName"][0];
+                        samAccountNames.Add(samAccountName);
+                    }
+
+                }
+
+                return samAccountNames;
+            }
+            catch (Exception ex)
+            {
+                UsingStreamWriter(ex.Message);
+                return null;
+            }
+
+        }
+    }
+
+
+    public static List<string> CutNullData(string[] anyArray)
+    {
+        List<string> cat = new List<string>();
+        for (int i = 0; i < anyArray.Length; i++)
+        {
+            if (anyArray[i] != null)
+            {
+                cat.Add(anyArray[i].ToString());
+            }
+        }
+        return cat;
+    }
+
+    public static List<string> GetSqlAllMail(string coccectToSQL)
+    {
+        List<string> result = new List<string>();
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(coccectToSQL))
+            {
+                connection.Open();
+                string queryText = "SELECT email FROM identities";
+                using (SqlCommand command = new SqlCommand(queryText, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            if (!reader.IsDBNull(0))
+                            {
+                                result.Add(reader.GetString(0));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            UsingStreamWriter(ex.Message);
+        }
+        return result;
+    }
+
+
+    public static void FindeNameDoesntExistAd(string coccectToSQL, string mail, string cn)
+    {
+        List<string> nameFromSql = new List<string>();
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(coccectToSQL))
+            {
+                connection.Open();
+                string queryText = $"SELECT name FROM identities WHERE email=\'{mail}\'";
+                using (SqlCommand command = new SqlCommand(queryText, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            nameFromSql.Add(reader.GetString(0));
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < nameFromSql.Count; i++)
+            {
+                if (nameFromSql[i] != cn)
+                {
+                    UsingStreamWriter("mail=" + mail);
+                    UsingStreamWriter("В AD cn=" + cn);
+                    UsingStreamWriter("В SQL name=" + nameFromSql[i]);
+                    UsingStreamWriter("");
+                }
+            }
+
+        }
+        catch (Exception ex)
+        {
+            UsingStreamWriter(ex.Message);
+        }
+    }
+
+}
+
---
+++ UpdateSignatureForAdmins/UpdateSignatureForAdmins/Program.cs
@@ -0,0 +1,270 @@
+﻿
+using static Const;
+using static Functions;
+
+Directory.CreateDirectory("./log");
+
+string sqlServerName;
+string sqlNameDB;
+string sqlUserName;
+string sqlPassword;
+string ldapPath;
+string adTagMailIgnor;
+string company;
+string streetAddress;
+string city;
+string telephoneNumber;
+string website;
+string b2c;
+string websiteLink;
+string imgLink;
+
+using (StreamReader ReaderObject = new StreamReader(configFilelway))
+{
+    string[] fileData = new string[] { };
+    fileData = ReaderObject.ReadToEnd().Split('\n');
+    sqlServerName = GetParametrValue(fileData[0], " ")[1];
+    sqlNameDB = GetParametrValue(fileData[1], " ")[1];
+    sqlUserName = GetParametrValue(fileData[2], " ")[1];
+    sqlPassword = GetParametrValue(fileData[3], " ")[1];
+    ldapPath = GetParametrValue(fileData[4], " ")[1];
+    adTagMailIgnor = GetParametrValue(fileData[5], " ")[1];
+    company = GetParametrValue(fileData[6], ": ")[1];
+    streetAddress = GetParametrValue(fileData[7], ": ")[1];
+    city = GetParametrValue(fileData[8], ": ")[1];
+    telephoneNumber = GetParametrValue(fileData[9], ": ")[1];
+    website = GetParametrValue(fileData[10], ": ")[1];
+    b2c = GetParametrValue(fileData[11], ": ")[1];
+    websiteLink = GetParametrValue(fileData[12], ": ")[1];
+    imgLink = GetParametrValue(fileData[13], ": ")[1];
+}
+
+string connectToSQL =
+$"Data Source={sqlServerName};" +
+    $"Initial Catalog={sqlNameDB};" +
+    $"User ID={sqlUserName};" +
+    $"Password={sqlPassword};" +
+    $"Encrypt=false;" +
+    $"TrustServerCertificate=true";
+
+string userUnsver = "";
+while (userUnsver != "exit")
+{
+    Dictionary<string, string> userData = new Dictionary<string, string>
+        {
+            {"cn", "" },
+            {"title", "" },
+            {"company", "" },
+            {"streetAddress", "" },
+            {"l", "" },
+            {"mail", "" },
+            {"telephoneNumber", "" },
+            {"sAMAccountName", "" },
+            {"mobile", "" },
+            {"pager", "" }
+        };
+    Console.WriteLine("||===================================================||");
+    Console.WriteLine("|| Обновляем подпись, или формируем списки?          ||");
+    Console.WriteLine("|| 1. Обновляем подпись                              ||");
+    Console.WriteLine("|| 2. Форируем списки                                ||");
+    Console.WriteLine("|| Вводи номер действия или напиши exit, чтобы выйти ||");
+    Console.WriteLine("||===================================================||");
+    Console.Write("|| ");
+    userUnsver = Console.ReadLine();
+
+    if (userUnsver == "1")
+    {
+        Console.WriteLine("||===================================================||");
+        Console.WriteLine("|| Напиши логин юзера                                ||");
+        Console.Write("|| ");
+        userData["sAMAccountName"] = Console.ReadLine();
+        Console.WriteLine("||===================================================||");
+
+        userData = GetAdUserAtributs(ldapPath, userData["sAMAccountName"]);
+        if (userData["pager"] != adTagMailIgnor && userData["sAMAccountName"] != "tyty")
+        {
+            string identity_id = GetSqlIdentityID(connectToSQL, userData["cn"], userData["mail"]);
+
+            string signatureText = "";
+            if (identity_id != null)
+            {
+                if (userData["mobile"] == null)
+                {
+                    signatureText = openDiv + openSpan + "С уважением," + closeSpan + closeDiv + "\r\n" +
+                        openDiv + openSpan + userData["cn"] + closeSpan + closeDiv + "\r\n" +
+                        openDiv + openSpan + userData["title"] + closeSpan + closeDiv + "\r\n" +
+                        openDiv + openSpan + userData["company"] + closeSpan + closeDiv + "\r\n" +
+                        openDiv + openSpan + userData["l"] + ", " + userData["streetAddress"] + closeSpan + closeDiv + "\r\n" +
+                        openDiv + openSpan + "Mail:         " + closeSpan +
+                        openSpan + openStrong + "<a href=\"mailto:" + userData["mail"] + "\">" + userData["mail"] + "</a>" + closeStrong + closeSpan + closeDiv + "\r\n" +
+                        openDiv + openSpan + "Phone:      8 (800)-222-52-62 доп:" + userData["telephoneNumber"] + "  " + closeSpan + closeDiv + "\r\n" +
+                        openDiv + openSpan + openStrong + "website     " + "<a href=\"https://" + website + "\" rel=\"noopener\">" + website + "</a>" + closeStrong + closeSpan + closeDiv + "\r\n" +
+                        openDiv + openSpan + openStrong + "b2c          " + "<a href=\"https://" + b2c + "\" rel=\"noopener\">" + b2c + "</a>" + closeStrong + closeSpan + closeDiv + "\r\n" +
+                        openDiv + closeDiv + "\r\n" + openDiv + "<a href=\"https://" + websiteLink + "\" rel=\"noopener noreferrer\">" + "<img src=\"https://" + imgLink + "\" />" + "</a>" + closeDiv;
+                }
+                else
+                {
+                    signatureText = openDiv + openSpan + "С уважением," + closeSpan + closeDiv + "\r\n" +
+                        openDiv + openSpan + userData["cn"] + closeSpan + closeDiv + "\r\n" +
+                        openDiv + openSpan + userData["title"] + closeSpan + closeDiv + "\r\n" +
+                        openDiv + openSpan + userData["company"] + closeSpan + closeDiv + "\r\n" +
+                        openDiv + openSpan + userData["l"] + ", " + userData["streetAddress"] + closeSpan + closeDiv + "\r\n" +
+                        openDiv + openSpan + "Mail:         " + closeSpan +
+                        openSpan + openStrong + "<a href=\"mailto:" + userData["mail"] + "\">" + userData["mail"] + "</a>" + closeStrong + closeSpan + closeDiv + "\r\n" +
+                        openDiv + openSpan + "Phone:      8 (800)-222-52-62 доп:" + userData["telephoneNumber"] + "  " + closeSpan + closeDiv + "\r\n" +
+                        openDiv + openSpan + "Mobile:      " + userData["mobile"] + "  " + closeSpan + closeDiv + "\r\n" +
+                        openDiv + openSpan + openStrong + "website     " + "<a href=\"https://" + website + "\" rel=\"noopener\">" + website + "</a>" + closeStrong + closeSpan + closeDiv + "\r\n" +
+                        openDiv + openSpan + openStrong + "b2c          " + "<a href=\"https://" + b2c + "\" rel=\"noopener\">" + b2c + "</a>" + closeStrong + closeSpan + closeDiv + "\r\n" +
+                        openDiv + closeDiv + "\r\n" + openDiv + "<a href=\"https://" + websiteLink + "\" rel=\"noopener noreferrer\">" + "<img src=\"https://" + imgLink + "\" />" + "</a>" + closeDiv;
+                }
+
+                if (identity_id != "" && GetSqlSignature(connectToSQL, identity_id) != signatureText)
+                {
+                    Console.WriteLine("|| Сформированная для identity_id=" + identity_id + " подпись не совпадает с полученной - перезаписываем.");
+                    UpdateSqLSignature(connectToSQL, signatureText, identity_id);
+                }
+                else
+                {
+                    Console.WriteLine("|| У пользователя и так правильная подпись           ||");
+                }
+            }
+
+        }
+        else
+        {
+            if (userData["pager"] == adTagMailIgnor)
+            {
+                Console.WriteLine("Этому пользователю нельзя менять подпись, у него pager=" + adTagMailIgnor);
+            }
+            if (userData["sAMAccountName"] == "tyty")
+            {
+                Console.WriteLine("|| Нет такого пользователя, ты шо там накалякал?     ||");
+            }
+        }
+
+        //Console.WriteLine("Хочешь выйти, пиши exit, хочешь повторить, пиши любую байду");
+    }
+
+    if (userUnsver == "2")
+    {
+        List<Dictionary<string, string>> userDataList = new List<Dictionary<string, string>>();
+        List<string> adLoginList = new List<string>();
+        adLoginList = CutNullData(GetAllLoginAd(ldapPath).ToArray());
+
+        for (int i = 0; i < adLoginList.Count; i++)
+        {
+            Dictionary<string, string> userDataCopy = new Dictionary<string, string>(userData);
+            userDataCopy["sAMAccountName"] = adLoginList[i];
+            userDataList.Add(userDataCopy);
+        }
+
+        for (int i = 0; i < userDataList.Count; i++)
+        {
+            Dictionary<string, string> userDataSearch = GetAdUserAtributs(ldapPath, userDataList[i]["sAMAccountName"]);
+
+            userDataList[i]["cn"] = userDataSearch["cn"];
+            userDataList[i]["title"] = userDataSearch["title"];
+            userDataList[i]["company"] = userDataSearch["company"];
+            userDataList[i]["streetAddress"] = userDataSearch["streetAddress"];
+            userDataList[i]["mail"] = userDataSearch["mail"];
+            userDataList[i]["telephoneNumber"] = userDataSearch["telephoneNumber"];
+            userDataList[i]["mobile"] = userDataSearch["mobile"];
+            userDataList[i]["l"] = userDataSearch["l"];
+            userDataList[i]["pager"] = userDataSearch["pager"];
+
+            if (userDataList[i]["company"] == null)
+            {
+                userDataList[i]["company"] = "ООО «Компания В.И.К»";
+            }
+            if (userDataList[i]["streetAddress"] == null)
+            {
+                userDataList[i]["streetAddress"] = "ул. Ростовское шоссе, 66";
+            }
+            if (userDataList[i]["l"] == null)
+            {
+                userDataList[i]["l"] = "г. Краснодар";
+            }
+            if (userDataList[i]["telephoneNumber"] == null)
+            {
+                userDataList[i]["telephoneNumber"] = telephoneNumber;
+            }
+        }
+
+        userDataList.RemoveAll(dict => (dict["mail"] == null));
+
+        UsingStreamWriter("||=============Email, которые следует пропустить==============||");
+        UsingStreamWriter("||====================так как pager=" + adTagMailIgnor  + "====================||");
+        for (int i = 0; i < userDataList.Count; i++)
+        {
+            if (userDataList[i]["pager"] == adTagMailIgnor)
+            {
+                UsingStreamWriter("cn=" + userDataList[i]["cn"]);
+                UsingStreamWriter("mail=" + userDataList[i]["mail"]);
+                UsingStreamWriter("");
+            }
+        }
+
+        userDataList.RemoveAll(dict => (dict["pager"] == adTagMailIgnor));
+
+        UsingStreamWriter("||====Выносим юзеров с одинаковыми mail в отдельный список====||");
+        UsingStreamWriter("");
+        var allDuplicates = userDataList
+            .GroupBy(dict => dict["mail"])
+            .Where(group => group.Count() > 1)
+            .SelectMany(group => group)
+            .ToList();
+        for (int i = 0; i < allDuplicates.Count; i++)
+        {
+            UsingStreamWriter("cn=" + allDuplicates[i]["cn"]);
+            UsingStreamWriter("mail=" + allDuplicates[i]["mail"]);
+            UsingStreamWriter("");
+        }
+
+        for (int i = 0; i < allDuplicates.Count; i++)
+        {
+            userDataList.RemoveAll(dict => (dict["mail"] == allDuplicates[i]["mail"]));
+        }
+
+        UsingStreamWriter("||===============Список mail, которых нет в AD================||");
+        List<string> mailFromSql = GetSqlAllMail(connectToSQL);
+        for (int i = 0; i < mailFromSql.Count; i++)
+        {
+            bool x = false;
+            for (int j = 0; j < userDataList.Count; j++)
+            {
+                if (mailFromSql[i] == userDataList[j]["mail"])
+                {
+                    x = true;
+                }
+            }
+            if (x == false)
+            {
+                UsingStreamWriter(mailFromSql[i]);
+            }
+        }
+
+        UsingStreamWriter("||===========У следующих mail не совпадает ФИО с AD===========||");
+        for (int i = 0; i < userDataList.Count; i++)
+        {
+            FindeNameDoesntExistAd(connectToSQL, userDataList[i]["mail"], userDataList[i]["cn"]);
+        }
+
+        Console.WriteLine("|| Информацию ищи в файле log\\log.txt                ||");
+    }
+
+    if (userUnsver != "1" && userUnsver != "2" && userUnsver != "exit")
+    {
+        Console.WriteLine("||===================================================||");
+        Console.WriteLine("|| Сам ты " + userUnsver);
+        Console.WriteLine("|| Напиши 1 или 2, в зависимости от того, что хочешь ||");
+        Console.WriteLine("|| сделать!                                          ||");
+        Console.WriteLine("|| Чтобы выйти, напиши exit                          ||");
+        Console.WriteLine("||===================================================||");
+    }
+
+    if (userUnsver == "exit")
+    {
+
+    }
+    
+}
